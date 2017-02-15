09942444cba3ef36427d723c2afa87804688c991
alumno <alumno@a51-07.local>
2017-02-14 19:12:15 -0500

    Initial Commit

---
+++ Responsive in iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Responsive in iOS
+//
+//  Created by alumno on 14/02/17.
+//  Copyright © 2017 Jorge Luis Limo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
